7ed0fb9cb4199b2b37f50889138b1b1b78334248
SirAmonThe1 <64291960+SirAmonThe1@users.noreply.github.com>
2022-04-22 18:04:43 +0200

    Pswu version 2.2.0.3

--- 10_modules/sysinfos.psm1
+++ 10_modules/sysinfos.psm1
@@ -107,16 +107,16 @@ function get-sysPSWindowsUpdateVersion {
     $PSWU | Format-Table -Property Name,Version
 
     do{
-	    if ($PSWU.Version -eq "2.2.0.2") {  Write-Host -ForegroundColor Green ">>> PSWindowsUpdate ist aktuell"  } 
+	    if ($PSWU.Version -eq "2.2.0.3") {  Write-Host -ForegroundColor Green ">>> PSWindowsUpdate ist aktuell"  } 
             else {
                 update-PSWindowsUpdatex
 
                 $PSWU = Get-Package -Name PSWindowsUpdate
 
-                if ($PSWU.Version -eq "2.2.0.2") {  Write-Host -ForegroundColor Green ">>> PSWindowsUpdate ist aktuell" } 
+                if ($PSWU.Version -eq "2.2.0.3") {  Write-Host -ForegroundColor Green ">>> PSWindowsUpdate ist aktuell" } 
                     else {
                         Write-Host -ForegroundColor DarkGray ($PSWU | Format-Table | Out-String)
-		                Write-Host -ForegroundColor Magenta ">>> Bitte PSWindowsUpdate über .bat Datei installieren"
+		                Write-Host -ForegroundColor Magenta ">>> Bitte PSWindowsUpdate Ã¼ber .bat Datei installieren"
                         x = $modulesPath + "\PSWindosUpdate Installieren"
                         ii x
 
@@ -132,7 +132,7 @@ function get-sysPSWindowsUpdateVersion {
 		    }
 	    }
 
-    while	($PSWU.Version -ne "2.2.0.2")
+    while	($PSWU.Version -ne "2.2.0.3")
 
 }
 
